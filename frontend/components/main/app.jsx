50e38107b7e4832ba93d355aae5677727dacc9f9
Ryan Mease <rsmease@gmail.com>
2018-01-09 17:48:12 -0800

    Draft projec index

--- frontend/components/main/app.jsx
+++ frontend/components/main/app.jsx
@@ -6,12 +6,14 @@ import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 //components
 import Splash from './splash.jsx';
 import Resume from '../resume/resume_container';
+import ProjectIndex from '../projects/project_index';
 
 
 const App = () => (
     <Switch>
         <Route exact path="/" component={Splash} />
         <Route exact path="/resume" component={Resume} />
+        <Route exact path="/projects" component={ProjectIndex} />
     </Switch>
 );
 
---
+++ frontend/components/projects/project_index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import * as FontAwesome from 'react-icons/lib/fa';
+import { Router, Link } from 'react-router-dom';
+
+class ProjectIndex extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="project-index-container">
+                <h1 className="project-title">Projects</h1>
+                <div className="project-index-item fade-in-first">
+                    <div className="project-index-item-slide-container">
+                        <div className="project-index-item-slide-icon">
+                            <h1 className="projext-index-item-big-title">Latin for Addicts</h1>
+                            <h1 className="projext-index-item-little-title">2012</h1>
+                            <p className="project-index-item-description">
+                                A Wordpress site <span className="great-content">used by hundreds of educators world wide</span>.
+                                This was my first foray into hosting content on the world wide web.
+                                I digitized an old but popular Latin textbook, summarizing articles on topics ranging from <span className="great-content">morphology to syntax to etymological histories</span>.
+                            </p>
+                            <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                        </div>
+                    </div>
+                </div>
+                <div className="project-index-item fade-in-second">
+                    <div className="project-index-item-slide-container">
+                        <div className="project-index-item-slide-icon">
+                            <h1 className="projext-index-item-big-title">Ashai</h1>
+                            <h1 className="projext-index-item-little-title">2017</h1>
+                            <p className="project-index-item-description">
+                                A <span className="great-content">React on Rails</span> site that highlights my latest skills.
+                                The app makes use of a <span className="great-content">Redux store and PostgreSQL API</span>.
+                                User authentication is managed on the front-end.
+                                <span className="great-content">The UI was built from the ground up</span>, with minimal reliance on third-party package templates.
+                            </p>
+                            <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProjectIndex;
