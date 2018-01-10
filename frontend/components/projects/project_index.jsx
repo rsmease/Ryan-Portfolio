ccc18958ad3e84b2174edc781768aa4f2e69d38c
Ryan Mease <rsmease@gmail.com>
2018-01-10 13:35:41 -0800

    Add diagonal lines to project index; add background to one project

--- frontend/components/projects/project_index.jsx
+++ frontend/components/projects/project_index.jsx
@@ -12,9 +12,12 @@ class ProjectIndex extends React.Component {
         return (
             <div className="project-index-container">
                 <div className="diagonal-line-container">
-                    <div className="diagonal-line"></div>
+                    <div className="diagonal-line-1"></div>
                 </div>
-                <h1 className="project-title">Projects</h1>
+                <div className="diagonal-line-container">
+                    <div className="diagonal-line-2"></div>
+                </div>
+                <h1 className="project-index-title">Projects</h1>
                 <div className="project-index-item fade-in-first">
                     <div className="project-index-item-slide-container">
                         <div className="project-index-item-slide-icon">
@@ -29,22 +32,27 @@ class ProjectIndex extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="project-index-item fade-in-second">
-                    <div className="project-index-item-slide-container">
-                        <div className="project-index-item-slide-icon">
-                            <h1 className="projext-index-item-big-title">Ashai</h1>
-                            <h1 className="projext-index-item-little-title">2017</h1>
-                            <p className="project-index-item-description">
-                                A <span className="great-content">React on Rails</span> site that highlights my latest skills.
+                <div className="project-index-item-container">
+                    <h1 className="project-index-item-title fade-in-second">Ashai</h1>
+                    <div className="project-index-item fade-in-second ashai">
+                        <div className="project-index-item-overlay">
+                            <a className="view-project-button" href="http://www.ashai.co">View Site</a>
+                            <div className="project-index-item-slide-container">
+                                <div className="project-index-item-slide-icon">
+                                    <p className="project-index-item-description">
+                                        A <span className="great-content">React on Rails</span> site that highlights my latest skills.
                                 The app makes use of a <span className="great-content">Redux store and PostgreSQL API</span>.
                                 User authentication is managed on the front-end.
                                 <span className="great-content">The UI was built from the ground up</span>, with minimal reliance on third-party package templates.
                             </p>
-                            <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                                    <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </div >
                 </div>
-            </div>
+
+            </div >
         );
     }
 }
