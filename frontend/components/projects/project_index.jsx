54b7ae2f4a748c1b6e1349c933588fda0dddf172
Ryan Mease <rsmease@gmail.com>
2018-01-10 14:09:57 -0800

    Final edits to projecti index, first draft

--- frontend/components/projects/project_index.jsx
+++ frontend/components/projects/project_index.jsx
@@ -18,20 +18,25 @@ class ProjectIndex extends React.Component {
                     <div className="diagonal-line-2"></div>
                 </div>
                 <h1 className="project-index-title">Projects</h1>
-                <div className="project-index-item fade-in-first">
-                    <div className="project-index-item-slide-container">
-                        <div className="project-index-item-slide-icon">
-                            <h1 className="projext-index-item-big-title">Latin for Addicts</h1>
-                            <h1 className="projext-index-item-little-title">2012</h1>
-                            <p className="project-index-item-description">
-                                A Wordpress site <span className="great-content">used by hundreds of educators world wide</span>.
+                <div className="project-index-item-container">
+                    <h1 className="project-index-item-title fade-in-first">Latin for Addicts</h1>
+                    <div className="project-index-item fade-in-first latinforaddicts">
+                        <div className="project-index-item-overlay">
+                            <a className="view-project-button" href="http://www.ashai.co">View Site</a>
+                            <div className="project-index-item-slide-container">
+                                <div className="project-index-item-slide-icon">
+                                    <p className="project-index-item-description">
+                                        A Wordpress site <span className="great-content">used by hundreds of educators world wide</span>.
                                 This was my first foray into hosting content on the world wide web.
                                 I digitized an old but popular Latin textbook, summarizing articles on topics ranging from <span className="great-content">morphology to syntax to etymological histories</span>.
                             </p>
-                            <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                                    <FontAwesome.FaAngleDoubleDown className="project-index-item-arrow-down" />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <div className="project-index-item-container">
                     <h1 className="project-index-item-title fade-in-second">Ashai</h1>
                     <div className="project-index-item fade-in-second ashai">
