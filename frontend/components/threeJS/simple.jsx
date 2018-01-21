bde38303f6d081713474ea184abcf9e45b91a00d
Ryan Mease <rsmease@gmail.com>
2018-01-21 15:43:16 -0800

    Fiddle with Three.js animation

--- frontend/components/threeJS/simple.jsx
+++ frontend/components/threeJS/simple.jsx
@@ -22,59 +22,59 @@ class Simple extends React.Component {
             1000
         );
         const renderer = new THREE.WebGLRenderer({ antialias: true });
-        const geometry = new THREE.SphereGeometry(1, 5, 5);
-        const material = new THREE.MeshBasicMaterial({ color: '#FFFFFF' });
+        const geometry = new THREE.SphereGeometry(1, 32, 32);
+        const material = new THREE.MeshDepthMaterial();
         const cube = new THREE.Mesh(geometry, material);
 
-        camera.position.z = 4
-        scene.add(cube)
-        renderer.setClearColor('#000000')
-        renderer.setSize(width, height)
+        camera.position.z = 4;
+        scene.add(cube);
+        renderer.setClearColor('#000000');
+        renderer.setSize(width, height);
 
-        this.scene = scene
-        this.camera = camera
-        this.renderer = renderer
-        this.material = material
-        this.cube = cube
+        this.scene = scene;
+        this.camera = camera;
+        this.renderer = renderer;
+        this.material = material;
+        this.cube = cube;
 
-        this.mount.appendChild(this.renderer.domElement)
-        this.start()
+        this.mount.appendChild(this.renderer.domElement);
+        this.start();
     }
 
     componentWillUnmount() {
-        this.stop()
-        this.mount.removeChild(this.renderer.domElement)
+        this.stop();
+        this.mount.removeChild(this.renderer.domElement);
     }
 
     start() {
         if (!this.frameId) {
-            this.frameId = requestAnimationFrame(this.animate)
+            this.frameId = requestAnimationFrame(this.animate);
         }
     }
 
     stop() {
-        cancelAnimationFrame(this.frameId)
+        cancelAnimationFrame(this.frameId);
     }
 
     animate() {
-        this.cube.rotation.x += 0.01
-        this.cube.rotation.y += 0.01
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
 
-        this.renderScene()
-        this.frameId = window.requestAnimationFrame(this.animate)
+        this.renderScene();
+        this.frameId = window.requestAnimationFrame(this.animate);
     }
 
     renderScene() {
-        this.renderer.render(this.scene, this.camera)
+        this.renderer.render(this.scene, this.camera);
     }
 
     render() {
         return (
             <div
-                style={{ width: '400px', height: '400px' }}
-                ref={(mount) => { this.mount = mount }}
+                style={{ width: "100vw", height: "100vh" }}
+                ref={(mount) => { this.mount = mount; }}
             />
-        )
+        );
     }
 }
 
