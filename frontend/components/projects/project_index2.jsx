a46c12f38bbccb20c6e482e37ebf5b674b70d775
Ryan Mease <rsmease@gmail.com>
2018-01-26 15:15:47 -0800

    Update draft, new project index

---
+++ frontend/components/projects/project_index2.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as FontAwesome from 'react-icons/lib/fa';
+import { Router, Link } from 'react-router-dom';
+
+class ProjectIndex extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="project-index-container">
+                <div className="diagonal-line-container">
+                    <div className="diagonal-line-1"></div>
+                </div>
+                <div className="diagonal-line-container">
+                    <div className="diagonal-line-2"></div>
+                </div>
+                <h1 className="project-index-title">Projects</h1>
+
+                <div className="project-index-item-container">
+                    <h1 className="project-index-item-title fade-in-first">Latin for Addicts</h1>
+                    <div className="project-index-item fade-in-first latinforaddicts">
+                        <div className="project-index-item-overlay">
+                            <a className="view-project-button" href="https://latinforaddicts.wordpress.com" target="_blank">View Site</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default ProjectIndex;
